4a0e7cff9841c93360e1d6a03ff08a7b102b84e4
哒哒哒 <17779916+malegedandan@users.noreply.github.com>
2023-11-21 13:43:29 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     .binaryTarget(
       name: "XrayKit",
       url: "https://github.com/xby023/XrayKit/archive/refs/tags/0.1.45.zip",
-      checksum: "541f9bf3f1b79f5c9f88899eab5f47c021ef7f0aa52c4373d9cb9cb236407afc"
+      // checksum: "541f9bf3f1b79f5c9f88899eab5f47c021ef7f0aa52c4373d9cb9cb236407afc"
     )
   ]
 )
